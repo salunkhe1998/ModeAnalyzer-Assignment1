ff6bbe226b2901a63617abd5db4138ca7db466d0
Ashwini <salunkheashwini1998@gmail.com>
2023-01-18 16:09:17 +0530

    Given massage ability to anyalyse and respond Happy or Sad mood

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeAnalyzerAssignment1
+{
+    public class MoodAnalyzer
+    {
+        public static string analyseMood(string message)
+        {
+
+            return message;
+        }
+    }
+}
