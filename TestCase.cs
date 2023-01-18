3dec89be44cc7a638ff9046f0057a67a795b246a
Ashwini <salunkheashwini1998@gmail.com>
2023-01-18 19:12:56 +0530

    Checking test case 1.2

--- TestCase.cs
+++ TestCase.cs
@@ -1,5 +1,4 @@
-﻿using ModeAnalyzerAssignment1;
-using ModeAnalyzerAssignment1;
+﻿using ModeAnalyzerAssignment;
 using System.Diagnostics.SymbolStore;
 
 namespace TestCaseMoodAnalyzer
@@ -21,47 +20,6 @@ namespace TestCaseMoodAnalyzer
             Assert.AreEqual(expected, actual);
         }
 
-        [TestMethod]
-        public void TestCase1_2()
-        {
-            string input = "I am in Any Mood";
-            string expected = "HAPPY";
 
-
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-
-            string actual = moodAnalyzer.analyseMood(input);
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void Refactor1_1()
-        {
-            string input = "I am in Sad Mood";
-            string expected = "SAD";
-
-
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
-
-            string actual = moodAnalyser.msg;
-
-            Assert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void Refactor1_2()
-        {
-            string input = "I am in Happy Mood";
-            string expected = "SAD";
-
-
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
-
-            string actual = moodAnalyser.msg;
-
-            Assert.AreEqual(expected, actual);
-        }
     }
 }
-
