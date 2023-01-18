a7d1a6369c99489d40b9bfc3571080cad28d6ddf
Ashwini <salunkheashwini1998@gmail.com>
2023-01-18 19:51:36 +0530

    testing the use case 2

---
+++ UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using ModeAnalyzerAssignment1;
+using ModeAnalyzerAssignment1;
+using System.Diagnostics.SymbolStore;
+
+namespace TestCaseMoodAnalyzer
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestCase1_1()
+        {
+            string input = "I am in Sad Mood";
+            string expected = "SAD";
+
+
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+
+            string actual = moodAnalyzer.analyseMood(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestCase1_2()
+        {
+            string input = "I am in Any Mood";
+            string expected = "HAPPY";
+
+
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+
+            string actual = moodAnalyzer.analyseMood(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+        [TestMethod]
+        public void UseCase2_1()
+        {
+            string input = "null";
+            string expected = "Happy";
+
+
+            MoodAnalyser moodAnalyser = new MoodAnalyser(input);
+
+            string actual = moodAnalyser.analyseMood1();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
