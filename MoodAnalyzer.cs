89fcacf7304159548b024180d27edc188b6195e5
Ashwini <salunkheashwini1998@gmail.com>
2023-01-18 19:08:24 +0530

    method calling

--- MoodAnalyzer.cs
+++ MoodAnalyzer.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.VisualBasic;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.SymbolStore;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ModeAnalyzerAssignment1
+namespace ModeAnalyzerAssignment
 {
     public class MoodAnalyzer
     {
@@ -26,28 +25,4 @@ namespace ModeAnalyzerAssignment1
         }
 
     }
-    public class MoodAnalyser
-    {
-
-        public string msg;
-        public MoodAnalyser()
-        {
-            this.msg = "SAD";
-        }
-        public MoodAnalyser(string msg1)
-        {
-            this.msg = msg1;
-
-        }
-
-        public string analyseMood1()
-        {
-            MoodAnalyser obja = new MoodAnalyser();
-            obja.msg = "HAPPY";
-            return obja.msg;
-
-        }
-
-    }
-
 }
