7632ee43deab20110c4174407e58919a102b0bff
Ashwini <salunkheashwini1998@gmail.com>
2023-01-18 16:42:46 +0530

    Updated

--- MoodAnalyzer.cs
+++ MoodAnalyzer.cs
@@ -43,6 +43,7 @@ namespace ModeAnalyzerAssignment1
         public string analyseMood1()
         {
             MoodAnalyser obja = new MoodAnalyser();
+            obja.msg = "HAPPY";
             return obja.msg;
 
         }
