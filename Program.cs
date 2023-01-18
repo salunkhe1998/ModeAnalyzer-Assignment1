6973d5c5d28d8fff73d20de1fbff0c01ddd6eed2
Ashwini <salunkheashwini1998@gmail.com>
2023-01-18 16:59:50 +0530

    Main class to check test case 1.1

--- Program.cs
+++ Program.cs
@@ -7,19 +7,10 @@ namespace ModeAnalyzerAssignment1
         public static void Main(string[] args)
         {
             string message = "I am in Sad Mood";
+            string message1 = "I am in Any Mood";
             MoodAnalyzer moodAnalyzer1 = new MoodAnalyzer();
             moodAnalyzer1.analyseMood(message);
 
-            MoodAnalyser moodAnalyser = new MoodAnalyser();
-            string r2 = moodAnalyser.analyseMood1();
-
-            string r1 = moodAnalyser.msg;
-            Console.WriteLine(r2);
-
-            Console.WriteLine(r1);
-
-
         }
     }
-
 }
