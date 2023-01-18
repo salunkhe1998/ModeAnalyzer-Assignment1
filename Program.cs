d13055979e97c71707affcdb800dfffb7530e4f1
Ashwini <salunkheashwini1998@gmail.com>
2023-01-18 19:26:37 +0530

    method calling

--- Program.cs
+++ Program.cs
@@ -1,16 +1,23 @@
-﻿using ModeAnalyzerAssignment1;
-
-namespace ModeAnalyzerAssignment1
+﻿namespace ModeAnalyzerAssignment
 {
     public class Program
     {
         public static void Main(string[] args)
         {
             string message = "I am in Sad Mood";
-            string message1 = "I am in Any Mood";
             MoodAnalyzer moodAnalyzer1 = new MoodAnalyzer();
             moodAnalyzer1.analyseMood(message);
 
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            string r2 = moodAnalyser.analyseMood1();
+
+            string r1 = moodAnalyser.msg;
+            Console.WriteLine(r2);
+
+            Console.WriteLine(r1);
+
+
         }
     }
+
 }
