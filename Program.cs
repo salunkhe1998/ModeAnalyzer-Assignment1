fc6bf778e2cdd45e2f8969695ddf576961ebd622
Ashwini <salunkheashwini1998@gmail.com>
2023-01-18 16:01:17 +0530

    Method Caling

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ModeAnalyzerAssignment1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string message;
+            Random random = new Random();
+            int mood = random.Next(2);
+
+            if (mood == 1)
+            {
+                message = "Happy";
+            }
+            else
+            {
+                message = "Sad";
+
+            }
+            Console.WriteLine(MoodAnalyzer.analyseMood(message));
+        }
+    }
+}
