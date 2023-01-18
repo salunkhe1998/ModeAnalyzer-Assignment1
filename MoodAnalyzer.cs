74422aa9853825a57ebf2101c5b1e9e42ac45ecd
Ashwini <salunkheashwini1998@gmail.com>
2023-01-18 19:28:51 +0530

    mood analyzer for uc2

--- MoodAnalyzer.cs
+++ MoodAnalyzer.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.SymbolStore;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ModeAnalyzerAssignment
+namespace ModeAnalyzerAssignment1
 {
     public class MoodAnalyzer
     {
@@ -25,4 +26,36 @@ namespace ModeAnalyzerAssignment
         }
 
     }
+    public class MoodAnalyser
+    {
+
+        public string msg;
+
+        public MoodAnalyser(string msg1)
+        {
+            this.msg = msg1;
+
+        }
+
+        public string analyseMood1()
+        {
+            MoodAnalyser obja = new MoodAnalyser(msg);
+
+            if (obja.msg == null)
+            {
+
+
+
+                throw new MoodAnalyzerNullException("Invalid Mood");
+            }
+            else
+            {
+                Console.WriteLine("Message is not NULL" + obja.msg);
+            }
+
+            return obja.msg;
+        }
+
+    }
+
 }
