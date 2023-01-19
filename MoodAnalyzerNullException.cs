9d98c4d554a9a1c502435c38560577c93f6dcde4
Ashwini <salunkheashwini1998@gmail.com>
2023-01-19 15:23:49 +0530

    case 3.1

--- MoodAnalyzerNullException.cs
+++ MoodAnalyzerNullException.cs
@@ -7,11 +7,19 @@ using System.Threading.Tasks;
 namespace ModeAnalyzerAssignment1
 {
 
+
     public class MoodAnalyzerNullException : Exception
     {
-        public MoodAnalyzerNullException(string msg) : base(msg)
+        Exception_Type type;
+        public enum Exception_Type
         {
+            EMPTY_MOOD,
+            NULL_MOOD
+        }
 
+        public MoodAnalyzerNullException(Exception_Type type, string msg) : base(msg)
+        {
+            this.type = type;
         }
     }
 
